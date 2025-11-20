c21750a43b394a9b2431d60534866c63ed294244
Louisa Liu <166338803+LouisaFrancious@users.noreply.github.com>
2025-11-20 12:12:44 -0500

    Update decimal_converter_buggy.c
    Shorten to 50s

--- decimal_converter_buggy.c
+++ decimal_converter_buggy.c
@@ -3,22 +3,16 @@
 int main () {
     int decimalNumber;
     printf("Please input a POSITIVE decimal number to be converted to hexadecimal and binary:");
-
-    // read the decimal value from the user
     scanf("%d", &decimalNumber);
-
-    // arrays used to store digits before printing them in reverse order
     char binary[500], hexadecimal[100];
     int i = 0, j = 0;
     int temp = decimalNumber;
-    
     // simple check for the special case when the number is zero
     if (temp == 0) {
         printf("Hexadecimal = 0\n");
         printf("Binary = 0\n");
         return 0;
     }
-
     // convert decimal to hexadecimal, collecting digits from least significant
     while (temp > 0) {
         int remainder = temp % 16;
@@ -30,26 +24,22 @@ int main () {
         temp /= 16;
         i++;
     }
-    
     // convert decimal to binary, again starting from least significant bit
     while (temp > 0) {
         int remainder = temp % 2;
         if (remainder == 1) {
-            binary[j] = '1';
+            binary[j] = '1';// j is used as the length/index for the binary representation
         } else {
             binary[j] = '0';
         }
         temp /= 2;
     }
-    // j is used as the length/index for the binary representation
-
     // print hexadecimal digits in reverse to get the correct order
     printf("hexadecimal conversion = ");
     for(int k = i; k >=0; k --){
         printf("%c",hexadecimal[k]);
     }
-    printf("\n");
-    
+    printf("\n"); 
     // print binary digits in reverse to get the correct order
     printf("binary conversion = ");
     for(int k = j -1; k >=0; k--){
